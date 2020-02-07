66348555b7df123830c29be8054632d4b1f73c44
Eyvar Velázquez <eyvarvelazquez@gmail.com>
2020-02-07 13:32:40 -0600

    Verificando que no haya null en objEscuela

--- Program.cs
+++ Program.cs
@@ -18,48 +18,17 @@ namespace Escuelita
                  new Curso() { Nombre = "301" }
             };
 
-            /* Console.WriteLine($"Curso:{objEscuela.Cursos[0].Nombre} ID:{objEscuela.Cursos[0].ID} Jornada:{objEscuela.Cursos[0].Jornada}");
-            Console.WriteLine($"Curso:{objEscuela.Cursos[1].Nombre} ID:{objEscuela.Cursos[1].ID} Jornada:{objEscuela.Cursos[1].Jornada}");
-            Console.WriteLine($"Curso:{objEscuela.Cursos[2].Nombre} ID:{objEscuela.Cursos[2].ID} Jornada:{objEscuela.Cursos[2].Jornada}"); */
-
+            //objEscuela.Cursos = null;
             ImprimeCursosForEach(objEscuela);
 
-
         }
 
 
-        private static void ImprimeCursosWhile(Escuela escuela)
-        {
-            int contador = 0;
-            while (contador < escuela.Cursos.Length)
-            {
-                Console.WriteLine($"Curso:{escuela.Cursos[contador].Nombre} ID:{escuela.Cursos[contador].ID} Jornada:{escuela.Cursos[contador].Jornada}");
-                contador++;
-            }
-
-        }
-
-        private static void ImprimeCursosDoWhile(Escuela escuela)
-        {
-            int contador = 0;
-            do
-            {
-                Console.WriteLine($"Curso:{escuela.Cursos[contador].Nombre} ID:{escuela.Cursos[contador].ID} Jornada:{escuela.Cursos[contador].Jornada}");
-            } while (++contador < escuela.Cursos.Length);
-        }
-
-
-        private static void ImprimeCursosFor(Escuela escuela)
-        {
-            for(int i=0; i<escuela.Cursos.Length; i++)
-            {
-                Console.WriteLine($"Curso:{escuela.Cursos[i].Nombre} ID:{escuela.Cursos[i].ID} Jornada:{escuela.Cursos[i].Jornada}");
-            }
-        }
 
         private static void ImprimeCursosForEach(Escuela escuela)
         {
-            foreach(Curso item in escuela.Cursos)
+            if (escuela?.Cursos != null)
+            foreach (Curso item in escuela.Cursos)
             {
                 Console.WriteLine($"Curso:{item.Nombre} ID:{item.ID} Jornada:{item.Jornada}");
             }
